636a30aa450eba73f5b6af943b0a43c13fcfa182
GarrettJMU <garretthughes3@gmail.com>
2019-11-07 21:56:09 -0800

    edit more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import indeed from 'indeed-scraper'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      level: 'entry_level',
+      radius: '25',
+      query: ''
+    };
+  }
+
+  makeServerCall() {
+    let queryOptions = {
+      host: 'www.indeed.com',
+      query: this.state.query,
+      city: 'San Diego, CA',
+      radius: this.state.radius,
+      level: this.state.level,
+      jobType: 'fulltime',
+      maxAge: '7',
+      sort: 'date',
+      limit: 100
+    };
+
+    indeed.query(queryOptions).then(res => {
+      console.log(res); // An array of Job objects
+    });
+  }
+
+  render() {
+    return(
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Let's find some jobs, bruh
+            </p>
+            <div>
+              
+            </div>
+          </header>
+        </div>
+    )
+  }
 }
-
-export default App;
